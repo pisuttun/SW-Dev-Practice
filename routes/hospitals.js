d04652c0da13a0fdc1a8122d4e1bf47e8a071137
Pisuttun <pisuttun@gmail.com>
2023-03-15 22:34:34 +0700

    assignment 7

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -7,9 +7,12 @@ const {
     deleteHospital,
 } = require("../controllers/hospitals");
 
+const appointmentRouter = require("./appointments");
 const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
+
+router.use("/:hospitalId/appointments", appointmentRouter);
 
 router.route("/").get(getHospitals).post(protect, authorize("admin"), createHospital);
 router
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/hospitals", require("./routes/hospitals"));
+app.use("/api/v1/appointments", require("./routes/appointments"));
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT));
