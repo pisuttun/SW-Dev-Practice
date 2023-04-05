084f9a1356d5a12a7255e8a942b26ec99c1dc953
Pisuttun <pisuttun@gmail.com>
2023-04-05 14:16:30 +0700

    assignment 10

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,19 +23,23 @@ exports.register = async (req, res, next) => {
 // @route   POST /api/v1/auth/login
 // @access  Public
 exports.login = async (req, res, next) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(400).json({ success: false, msg: "Please provide an email and password" });
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ success: false, msg: "Please provide an email and password" });
+        }
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(400).json({ success: false, msg: "Invalid credentials" });
+        }
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ success: false, msg: "Invalid credentials" });
+        }
+        sendTokenResponse(user, 200, res);
+    } catch (err) {
+        return res.status(401).json({ success: false, msg: "Cannot convert email or password to string" });
     }
-    const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-        return res.status(400).json({ success: false, msg: "Invalid credentials" });
-    }
-    const isMatch = await user.matchPassword(password);
-    if (!isMatch) {
-        return res.status(401).json({ success: false, msg: "Invalid credentials" });
-    }
-    sendTokenResponse(user, 200, res);
 };
 
 //Get token from model, create cookie and send response
@@ -58,4 +62,15 @@ const sendTokenResponse = (user, statusCode, res) => {
 exports.getMe = async (req, res, next) => {
     const user = await User.findById(req.user.id);
     res.status(200).json({ success: true, data: user });
+};
+
+// @desc    Logout user / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = async (req, res, next) => {
+    res.cookie("token", "none", {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+    res.status(200).json({ success: true, data: {} });
 };
--- server.js
+++ server.js
@@ -2,14 +2,56 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Library API",
+            version: "1.0.0",
+            description: "A simple Express VacQ API",
+        },
+        servers: [
+            {
+                url: "http://localhost:5000/api/v1",
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(cors());
+
+//Rate Limiting
+const limiter = rateLimit({
+    windowsMs: 10 * 60 * 1000, //10 mins
+    max: 100,
+});
+app.use(limiter);
 
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/hospitals", require("./routes/hospitals"));
